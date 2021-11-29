5e866511f6e79b984b559b297fe76e419986bf02
kally-the-great <kallythegreat11@gmail.com>
2021-11-29 16:28:08 +0100

    omo

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ from resources.user import registeruser
 from resources.item import Item,Itemlist
 from resources.store import Store,Storelist
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL","sqlite:///data.db")
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get(uri,"sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.secret_key="kally"
 api=Api(app)
